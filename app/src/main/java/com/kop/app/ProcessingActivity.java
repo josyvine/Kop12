05250babdf98c47b8bb35032958c7c163c592bdc
josyvine <64621103+josyvine@users.noreply.github.com>
2025-09-17 22:42:12 +0530

    Update ProcessingActivity.java

--- app/src/main/java/com/kop/app/ProcessingActivity.java
+++ app/src/main/java/com/kop/app/ProcessingActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.os.Environment;
+import android.os.Environment; 
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
