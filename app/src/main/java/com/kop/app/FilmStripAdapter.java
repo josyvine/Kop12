2f8efa39118513ddb1b3b182c8ef645420eb66c7
josyvine <64621103+josyvine@users.noreply.github.com>
2025-09-17 09:22:09 +0530

    Update FilmStripAdapter.java

--- app/src/main/java/com/kop/app/FilmStripAdapter.java
+++ app/src/main/java/com/kop/app/FilmStripAdapter.java
@@ -7,7 +7,7 @@ import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.ImageView; 
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
