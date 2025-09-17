80459c126f364a76b162982f07007ac857a57186
josyvine <64621103+josyvine@users.noreply.github.com>
2025-09-17 21:46:30 +0530

    Update DeepScanProcessor.java

--- app/src/main/java/com/kop/app/DeepScanProcessor.java
+++ app/src/main/java/com/kop/app/DeepScanProcessor.java
@@ -5,7 +5,7 @@ import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint; 
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
