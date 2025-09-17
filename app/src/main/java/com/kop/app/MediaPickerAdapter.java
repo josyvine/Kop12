9d0c1c3b475f6ebadfb2dfa6c5615bb90704b3cf
josyvine <64621103+josyvine@users.noreply.github.com>
2025-09-17 12:52:42 +0530

    Update MediaPickerAdapter.java

--- app/src/main/java/com/kop/app/MediaPickerAdapter.java
+++ app/src/main/java/com/kop/app/MediaPickerAdapter.java
@@ -9,7 +9,7 @@ import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
+import android.widget.FrameLayout; 
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
