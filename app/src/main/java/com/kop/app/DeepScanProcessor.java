33f2d4ee79cfd3d58194bb49f0b0057ffda23435
josyvine <64621103+josyvine@users.noreply.github.com>
2025-09-17 09:26:59 +0530

    Update DeepScanProcessor.java

--- app/src/main/java/com/kop/app/DeepScanProcessor.java
+++ app/src/main/java/com/kop/app/DeepScanProcessor.java
@@ -8,7 +8,7 @@ import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
-import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Imgproc; 
 import java.util.ArrayList;
 import java.util.List;
 
