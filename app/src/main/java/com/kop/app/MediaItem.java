c9ed9035a97a460cdd047ff0ae91351cafc3cc27
josyvine <64621103+josyvine@users.noreply.github.com>
2025-09-19 23:34:54 +0530

    Create MediaItem.java

---
+++ app/src/main/java/com/kop/app/MediaItem.java
@@ -0,0 +1,39 @@
+package com.kop.app;
+
+import java.io.File;
+
+public class MediaItem {
+
+    public enum ItemType {
+        PARENT, // Represents the ".." up directory
+        FOLDER,
+        FILE
+    }
+
+    private final File file;
+    private final ItemType type;
+
+    public MediaItem(File file, ItemType type) {
+        this.file = file;
+        this.type = type;
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public ItemType getType() {
+        return type;
+    }
+
+    public String getName() {
+        if (type == ItemType.PARENT) {
+            return "..";
+        }
+        return file != null ? file.getName() : "";
+    }
+
+    public String getPath() {
+        return file != null ? file.getAbsolutePath() : "";
+    }
+}
