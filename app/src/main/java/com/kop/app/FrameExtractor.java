a4a3264174b5d8274dff72cb604b8286da632a94
josyvine <64621103+josyvine@users.noreply.github.com>
2025-09-20 22:35:10 +0530

    Update FrameExtractor.java

--- app/src/main/java/com/kop/app/FrameExtractor.java
+++ app/src/main/java/com/kop/app/FrameExtractor.java
@@ -1,18 +1,18 @@
 package com.kop.app;
 
-import com.bihe0832.android.ffmpeg.FFmpegWrapper;
-import com.bihe0832.android.ffmpeg.OnFFmpegListener;
+import com.bihe0832.android.lib.aaf.tools.AAFDataCallback;
+import com.bihe0832.android.lib.aaf.wrapper.AAFWrapper;
 
 import java.io.File;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class FrameExtractor {
 
     /**
      * Extracts frames from a video file at a specified frames-per-second rate using the high-performance FFmpeg library.
      * @param videoPath Absolute path to the video file.
-     * @param outDir    The directory where the extracted frames will be saved.
+     * a* @param outDir    The directory where the extracted frames will be saved.
      * @param fps       The desired number of frames to extract per second.
      * @throws Exception if there is an error during extraction.
      */
@@ -22,60 +22,48 @@ public class FrameExtractor {
             outputDir.mkdirs();
         }
 
-        // This library requires the command to be split into an array of strings.
-        String[] command = new String[]{
-                "-i",
-                videoPath,
-                "-y", // Overwrite output files without asking
-                "-vf",
-                "fps=" + fps,
-                outDir + "/frame_%05d.png"
-        };
+        // This is the high-speed FFmpeg command as a single string.
+        // -i [videoPath]       -> Sets the input video file.
+        // -y                   -> Overwrite output files without asking.
+        // -vf fps=[fps]        -> Sets a video filter (-vf) to force the output frame rate to the desired fps.
+        // [outDir]/frame...png -> Sets the output location and filename pattern.
+        //                        %05d creates a 5-digit number (00001, 00002, etc.).
+        String command = String.format("-i \"%s\" -y -vf fps=%d \"%s/frame_%%05d.png\"", videoPath, fps, outDir);
 
         // This library is asynchronous. We use a CountDownLatch to make our method wait for it to finish.
         final CountDownLatch latch = new CountDownLatch(1);
-        final AtomicBoolean success = new AtomicBoolean(false);
-        final StringBuilder logBuilder = new StringBuilder();
+        final AtomicInteger returnCode = new AtomicInteger(-1); // Use AtomicInteger to store result from callback
 
-        // This is the real, correct way to execute a command with this library.
-        FFmpegWrapper.execute(command, new OnFFmpegListener() {
-            @Override
-            public void onStart() {
-                // Method is called when the command starts.
-            }
+        // THIS IS THE REAL, CORRECT WAY TO EXECUTE A COMMAND WITH THIS LIBRARY.
+        AAFWrapper.exec(command,
+                // onSuccess callback
+                new AAFDataCallback() {
+                    @Override
+                    public void onCallback(Object... args) {
+                        returnCode.set(0); // Set 0 for success
+                        latch.countDown();
+                    }
+                },
+                // onFailure callback
+                new AAFDataCallback() {
+                    @Override
+                    public void onCallback(Object... args) {
+                        if (args.length > 0 && args[0] instanceof Integer) {
+                            returnCode.set((Integer) args[0]);
+                        } else {
+                            returnCode.set(-1); // General failure
+                        }
+                        latch.countDown();
+                    }
+                }
+        );
 
-            @Override
-            public void onProgress(int frame, int totalFrame) {
-                // Method is called during processing, not needed here.
-            }
-
-            @Override
-            public void onSuccess() {
-                success.set(true);
-                latch.countDown(); // Signal that the command is finished.
-            }
-
-            @Override
-            public void onCancel() {
-                success.set(false);
-                logBuilder.append("FFmpeg command was cancelled.");
-                latch.countDown(); // Signal that the command is finished.
-            }
-
-            @Override
-            public void onFailure(String message) {
-                success.set(false);
-                logBuilder.append("FFmpeg command failed: ").append(message);
-                latch.countDown(); // Signal that the command is finished.
-            }
-        });
-
-        // Wait here until the latch is counted down by onSuccess, onFailure, or onCancel.
+        // Wait here until the latch is counted down by one of the callbacks.
         latch.await();
 
-        // Check if the command was successful. If not, throw an exception with the logs.
-        if (!success.get()) {
-            throw new Exception("FFmpeg frame extraction failed. Log: " + logBuilder.toString());
+        // Check if the command was successful. A return code of 0 means success.
+        if (returnCode.get() != 0) {
+            throw new Exception("FFmpeg frame extraction failed with return code: " + returnCode.get());
         }
     }
 }
