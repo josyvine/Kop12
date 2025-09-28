a71d68afed4a5eb20898f6549c493f4e599557e3
josyvine <64621103+josyvine@users.noreply.github.com>
2025-09-28 13:56:07 +0530

    Update vertex_shader.glsl

--- app/src/main/assets/vertex_shader.glsl
+++ app/src/main/assets/vertex_shader.glsl
@@ -1,8 +1,9 @@
 attribute vec4 vPosition;
 attribute vec2 vTexCoord;
-varying vec2 vTextureCoord; 
+varying vec2 vTextureCoord;
+uniform mat4 uTransformMatrix;
 
 void main() {
     gl_Position = vPosition;
-    vTextureCoord = vTexCoord;
+    vTextureCoord = (uTransformMatrix * vec4(vTexCoord, 0.0, 1.0)).xy;
 }
